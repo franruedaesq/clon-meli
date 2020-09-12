2ffa687039a6b14272b931d2be2e67ec6cba88fb
Francisco Rueda <franruedaesq@gmail.com>
2020-09-11 21:22:25 -0300

    Updates

---
+++ precache-manifest.d29e34c985c8b439237b7369df0b8d70.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff526800b170f823304014bd3fa8933",
+    "url": "/clon-meli/index.html"
+  },
+  {
+    "revision": "9d455398afedf22552ca",
+    "url": "/clon-meli/static/css/main.b423c4d3.chunk.css"
+  },
+  {
+    "revision": "1bc786513b348c22155e",
+    "url": "/clon-meli/static/js/2.4be00be0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/clon-meli/static/js/2.4be00be0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d455398afedf22552ca",
+    "url": "/clon-meli/static/js/main.9198edb0.chunk.js"
+  },
+  {
+    "revision": "35702bab9857db1b0141",
+    "url": "/clon-meli/static/js/runtime-main.03be6b11.js"
+  },
+  {
+    "revision": "da4bec6f7aeeaf44e212166c1465bf96",
+    "url": "/clon-meli/static/media/logo-ml.da4bec6f.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clon-meli/static/media/logo.5d5d9eef.svg"
+  }
+]);
