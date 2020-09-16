aaeace7dd82e46b626f3f6aa889624cdd06b2f99
Francisco Rueda <franruedaesq@gmail.com>
2020-09-16 00:03:43 -0300

    Add Component ButtonBuyItem

---
+++ src/components/ButtonBuyItem/ButtonBuyItem.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './ButtonBuyItem.scss'
+
+export default function ButtonBuyItem() {
+    return (
+        <button className="ButtonBuyItem ButtonBuyItem--blue">Comprar ahora</button>
+    )
+}
