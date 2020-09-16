5be20be3d8739852fbd4b6127708cc5eddb32e08
Francisco Rueda <franruedaesq@gmail.com>
2020-09-16 00:03:19 -0300

    Add component ButtonAddToBasket

---
+++ src/components/ButtonAddToBasket/ButtonAddToBasket.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './ButtonAddToBasket.scss'
+
+export default function ButtonAddToBasket() {
+    return (
+        <button className="ButtonAddToBasket ButtonAddToBasket--lightblue">Agregar al carrito</button>
+    )
+}
