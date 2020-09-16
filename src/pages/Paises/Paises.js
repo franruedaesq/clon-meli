44dbd773739fddc67038a96c1167bb599e0d6348
Francisco Rueda <franruedaesq@gmail.com>
2020-09-16 00:07:01 -0300

    fix to paises

--- src/pages/Paises/Paises.js
+++ src/pages/Paises/Paises.js
@@ -1,55 +1,54 @@
-import React, {useEffect, useState} from 'react'
-import Pais from '../../components/Pais/Pais'
-import './Paises.scss'
-import logoML from './logo-ml.png'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import Pais from "../../components/Pais/Pais";
+import "./Paises.scss";
+import logoML from "./logo-ml.png";
+import { Link } from "react-router-dom";
 
 export default function Paises() {
+  let [paisData, setPaisData] = useState([]);
 
-    let [paisData, setPaisData ] = useState([])
+  useEffect(() => {
+    fetchPaises();
+  }, []);
 
-    useEffect(() => {
-        fetchPaises()
-    }, [])
+  const fetchPaises = async () => {
+    let data = await fetch(
+      "https://cors-anywhere.herokuapp.com/https://api.mercadolibre.com/sites"
+    );
+    let jsonData = await data.json();
+    setPaisData(jsonData);
+  };
 
-    const fetchPaises = async() => {
-        let data = await fetch('https://cors-anywhere.herokuapp.com/https://api.mercadolibre.com/sites');
-        let jsonData = await data.json();
-        setPaisData(jsonData)
-        console.log(jsonData)
-    }
+  const listaPaises = [
+    "Argentina",
+    "Bolivia",
+    "Brasil",
+    "Chile",
+    "Colombia",
+    "Costa Rica",
+    "Cuba",
+    "Ecuador",
+    "El Salvador",
+    "Guatemala",
+    "Haití",
+    "Honduras",
+    "México",
+    "Nicaragua",
+    "Panamá",
+    "Paraguay",
+    "Perú",
+    "Uruguay",
+    "Venezuela",
+  ];
 
+  let lista = paisData.map((item) => (
+    <Pais meliID={item.id} name={item.name} key={item.id}></Pais>
+  ));
+  return (
+    <div className="containerPaises">
+      <img src={logoML} alt="" className="containerPaises__logo" />
 
-    const listaPaises = [
-        "Argentina",
-        "Bolivia",
-        "Brasil",
-        "Chile",
-        "Colombia",
-        "Costa Rica",
-        "Cuba",
-        "Ecuador",
-        "El Salvador",
-        "Guatemala",
-        "Haití",
-        "Honduras",
-        "México",
-        "Nicaragua",
-        "Panamá",
-        "Paraguay",
-        "Perú",
-        "Uruguay",
-        "Venezuela",
-    ]
-
-    let lista = paisData.map(item => <Pais meliID={item.id} name={item.name} key={item.id}></Pais>)
-    return (
-            <div className="containerPaises">
-                <img src={logoML} alt="" className="containerPaises__logo"/>
-
-                <div className="containerPaises__listaPaises">
-                {lista}
-                </div>
-            </div>
-    )
+      <div className="containerPaises__listaPaises">{lista}</div>
+    </div>
+  );
 }
