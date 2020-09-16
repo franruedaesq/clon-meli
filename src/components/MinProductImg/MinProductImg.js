753aa4ff903a0bef00c38f26d66365573b94a205
Francisco Rueda <franruedaesq@gmail.com>
2020-09-16 00:07:29 -0300

    min fix to component MinProductImg

--- src/components/MinProductImg/MinProductImg.js
+++ src/components/MinProductImg/MinProductImg.js
@@ -1,11 +1,15 @@
-import React from 'react'
-import './MinProductImg.scss'
+import React from "react";
+import "./MinProductImg.scss";
 
-export default function MinProductImg({srcImg, imgSelected}) {
-    console.log(srcImg)
-    return (
-        <>
-                <img onClick={imgSelected} className='minProductImg__item' src={srcImg} alt=""/>
-        </>
-    )
+export default function MinProductImg({ srcImg, imgSelected }) {
+  return (
+    <>
+      <img
+        onClick={imgSelected}
+        className="minProductImg__item"
+        src={srcImg}
+        alt=""
+      />
+    </>
+  );
 }
